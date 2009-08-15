bc2a93316862e6797c711891818db7e6ed1684e7
houdah.frameworks <houdah.frameworks@3a2ce6c6-89e1-11de-b74b-2df4680eaf45>
2009-08-15 21:25:52 +0000

    Initial import

---
+++ Source/OEEveryTrailAPIRequest.h
@@ -0,0 +1,126 @@
+//
+//  OEEveryTrailAPIRequest.h
+//
+// Copyright (c) 2009 Houdah Software s.à r.l. (http://www.houdah.com)
+// Copyright (c) 2009 Lukhnos D. Liu (http://lukhnos.org)
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "LFWebAPIKit.h"
+#import "OEUtilities.h"
+#import "OEXMLMapper.h"
+
+
+@class OEEveryTrailAPIContext;
+@protocol OEEveryTrailAPIRequestDelegate;
+
+
+extern NSString *const OEEveryTrailAPIReturnedErrorDomain;
+extern NSString *const OEEveryTrailAPIRequestErrorDomain;
+
+enum {
+	// refer to EveryTrail API document for EveryTrail's own error codes
+    OEEveryTrailAPIRequestConnectionError				= -1,
+    OEEveryTrailAPIRequestTimeoutError					= -2,    
+	OEEveryTrailAPIRequestFaultyXMLResponseError		= -3,
+	OEEveryTrailAPIRequestAuthenticationError			= -4,
+    OEEveryTrailAPIRequestUnknownError					= -100
+};
+
+@class OEEveryTrailAPIRequest;
+
+#if MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_4
+@protocol OEEveryTrailAPIRequestDelegate <NSObject>
+@optional
+#else
+@interface NSObject (OEEveryTrailAPIRequestDelegateCategory)
+#endif
+- (void)everyTrailAPIRequest:(OEEveryTrailAPIRequest *)inRequest didCompleteWithResponse:(NSDictionary *)inResponseDictionary;
+- (void)everyTrailAPIRequest:(OEEveryTrailAPIRequest *)inRequest didFailWithError:(NSError *)inError;
+#if MAC_OS_X_VERSION_MAX_ALLOWED > MAC_OS_X_VERSION_10_4                
+- (void)everyTrailAPIRequest:(OEEveryTrailAPIRequest *)inRequest imageUploadSentBytes:(NSUInteger)inSentBytes totalBytes:(NSUInteger)inTotalBytes;
+#else
+- (void)everyTrailAPIRequest:(OEEveryTrailAPIRequest *)inRequest imageUploadSentBytes:(unsigned int)inSentBytes totalBytes:(unsigned int)inTotalBytes;
+#endif
+@end
+
+#if MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_4
+typedef id<OEEveryTrailAPIRequestDelegate> OEEveryTrailAPIRequestDelegateType;
+#else
+typedef id OEEveryTrailAPIRequestDelegateType;
+#endif
+
+@interface OEEveryTrailAPIRequest : NSObject
+{
+    OEEveryTrailAPIContext *context;
+    LFHTTPRequest *httpRequest;
+    
+    OEEveryTrailAPIRequestDelegateType delegate;
+    id sessionInfo;
+    
+    NSString *uploadTempFilename;
+
+	NSString *userId;
+	NSInvocation *invocation;
+}
+
+- (id)initWithAPIContext:(OEEveryTrailAPIContext *)inContext;
+- (OEEveryTrailAPIContext *)context;
+
+- (OEEveryTrailAPIRequestDelegateType)delegate;
+- (void)setDelegate:(OEEveryTrailAPIRequestDelegateType)inDelegate;
+
+- (id)sessionInfo;
+- (void)setSessionInfo:(id)inInfo;
+
+- (NSTimeInterval)requestTimeoutInterval;
+- (void)setRequestTimeoutInterval:(NSTimeInterval)inTimeInterval;
+- (BOOL)isRunning;
+- (void)cancel;
+
+// elementary methods
+- (void)callAPIMethodWithGET:(NSString *)inMethodName
+					inDomain:(NSString *)inDomainName
+				   arguments:(NSDictionary *)inArguments
+			  authentication:(BOOL)inAuthentication;
+- (void)callAPIMethodWithPOST:(NSString *)inMethodName
+					 inDomain:(NSString *)inDomainName
+					arguments:(NSDictionary *)inArguments
+			   authentication:(BOOL)inAuthentication;
+
+// image upload—we use NSInputStream here because we want to have flexibity; with this you can upload either a file or NSData from NSImage
+- (void)uploadImageStream:(NSInputStream *)inImageStream
+		suggestedFilename:(NSString *)inFilename
+				 MIMEType:(NSString *)inType
+				arguments:(NSDictionary *)inArguments;
+
+#if MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_4
+@property (nonatomic, readonly) OEEveryTrailAPIContext *context;
+@property (nonatomic, assign) OEEveryTrailAPIRequestDelegateType delegate;
+@property (nonatomic, retain) id sessionInfo;
+@property (nonatomic, assign) NSTimeInterval requestTimeoutInterval;
+#endif
+
+@end
---
+++ Source/OEXMLMapper.m
@@ -0,0 +1,174 @@
+//
+// OEXMLMapper.m
+//
+// Copyright (c) 2009 Houdah Software s.à r.l. (http://www.houdah.com)
+// Copyright (c) 2009 Lukhnos D. Liu (http://lukhnos.org)
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "OEXMLMapper.h"
+
+NSString *OEXMLMapperExceptionName = @"OEXMLMapperException";
+NSString *OEXMLAttributesKey = @"_attributes";
+NSString *OEXMLTextContentKey = @"_text";
+
+@implementation OEXMLMapper
+- (void)dealloc
+{
+    [resultantDictionary release];
+	[elementStack release];
+	[currentElementName release];
+    [super dealloc];
+}
+
+- (id)init
+{
+    if (self = [super init]) {
+        resultantDictionary = [[NSMutableDictionary alloc] init];
+		elementStack = [[NSMutableArray alloc] init];
+    }
+    
+    return self;
+}
+
+- (void)runWithData:(NSData *)inData
+{
+	currentDictionary = resultantDictionary;
+	
+    NSXMLParser *parser = [[NSXMLParser alloc] initWithData:inData];
+	[parser setDelegate:self];
+	[parser parse];
+	[parser release];
+}
+
+- (NSDictionary *)resultantDictionary
+{
+	return [[resultantDictionary retain] autorelease];
+}
+
++ (NSDictionary *)dictionaryMappedFromXMLData:(NSData *)inData
+{
+	OEXMLMapper *mapper = [[OEXMLMapper alloc] init];
+	[mapper runWithData:inData];
+	NSDictionary *result = [mapper resultantDictionary];
+	[mapper release];
+	return result;
+}
+
+- (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict
+{
+	NSMutableDictionary *mutableDict = attributeDict ?  [NSMutableDictionary dictionaryWithObject:attributeDict forKey:OEXMLAttributesKey] : [NSMutableDictionary dictionary];
+	
+	// see if it's duplicated
+	id element;
+	if (element = [currentDictionary objectForKey:elementName]) {
+		if (![element isKindOfClass:[NSMutableArray class]]) {
+			if ([element isKindOfClass:[NSMutableDictionary class]]) {
+				[element retain];
+				[currentDictionary removeObjectForKey:elementName];
+				
+				NSMutableArray *newArray = [NSMutableArray arrayWithObject:element];
+				[currentDictionary setObject:newArray forKey:elementName];
+				[element release];
+				
+				element = newArray;
+			}
+			else {
+				@throw [NSException exceptionWithName:OEXMLMapperExceptionName reason:@"Faulty XML structure" userInfo:nil];
+			}
+		}
+		
+		[element addObject:mutableDict];
+	}
+	else {
+		// plural tag rule: if the parent's tag is plural and the incoming is singular, we'll make it into an array (we only handles the -s case)
+		
+		if ([currentElementName length] > [elementName length] && [currentElementName hasPrefix:elementName] && [currentElementName hasSuffix:@"s"]) {
+			[currentDictionary setObject:[NSMutableArray arrayWithObject:mutableDict] forKey:elementName];
+		}
+		else {
+			[currentDictionary setObject:mutableDict forKey:elementName];
+		}
+	}
+	
+	[elementStack insertObject:currentDictionary atIndex:0];
+	currentDictionary = mutableDict;
+	
+	NSString *tmp = currentElementName;
+	currentElementName = [elementName retain];
+	[tmp release];
+}
+
+- (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName
+{
+	if (![elementStack count]) {
+		@throw [NSException exceptionWithName:OEXMLMapperExceptionName reason:@"Unbalanced XML element tag closing" userInfo:nil];
+	}
+	
+	currentDictionary = [elementStack objectAtIndex:0];
+	[elementStack removeObjectAtIndex:0];
+}
+
+- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string
+{
+	NSString *existingString = [currentDictionary objectForKey:OEXMLTextContentKey];
+	
+	if (existingString != nil) {
+		[currentDictionary setObject:[existingString stringByAppendingString:string] forKey:OEXMLTextContentKey];
+	}
+	else {
+		[currentDictionary setObject:string forKey:OEXMLTextContentKey];
+	}
+}
+
+- (void)parser:(NSXMLParser *)parser parseErrorOccurred:(NSError *)parseError
+{
+	[resultantDictionary release];
+	resultantDictionary = nil;
+}
+@end
+
+@implementation NSDictionary (OEXMLMapperExtension)
+
+- (NSDictionary *)attributes
+{
+    return [self objectForKey:OEXMLAttributesKey];
+}
+
+- (NSString *)textContent
+{
+    return [self objectForKey:OEXMLTextContentKey];
+}
+
+- (NSObject *)rootElement
+{
+	if ([self count] == 1) {
+		NSObject *key = [[self allKeys] objectAtIndex:0];
+		
+		return [self objectForKey:key];
+	}
+	
+	return nil;
+}
+
+@end
